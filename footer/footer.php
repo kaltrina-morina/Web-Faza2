c1ed5e497b8323338e1db496e5ee848188d5a8cf
isakrexha1 <63118792+isakrexha1@users.noreply.github.com>
2022-04-21 15:02:14 -0700

    final

---
+++ footer/footer.php
@@ -0,0 +1,34 @@
+
+<section class="footer" id="footer">
+    <div class="footer-box">
+        <h2>Mo<span>Re.</span></h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt amet minus mollitia impedit qui asperiores!</p>
+        <div class="social">
+            <a href="#"><i class='bx bxl-facebook'></i></a>
+            <a href="#"><i class='bx bxl-twitter'></i></a>
+            <a href="#"><i class='bx bxl-instagram'></i></a>
+        </div>
+    </div>
+    <div class="footer-box">
+        <h3>Explore</h3>
+        <li><a href="../HomePage/index.php">Home</a></li>
+        <li><a href="../HomePage/about.php">About Us</a></li>
+        <li><a href="../Categories/categories.php">Courses</a></li>
+        <li><a href="contact.php">Contact</a></li>
+    </div>
+    <div class="footer-box">
+        <h3>Courses</h3>
+        <li><a href="#">Java</a></li>
+        <li><a href="#">Python</a></li>
+        <li><a href="#">JavaScript</a></li>
+        <li><a href="#">C++</a></li>
+    </div>
+    <div class="footer-box contact-info">
+        <h3>Contact</h3>
+        <span>Topanicë / Kamenicë, KOSOVË</span>
+        <span>+383 (45) 448 707</span>
+        <span>kaltrinaa.morinaa@gmail.com</span>
+        <span>rexhaisak@gmail.com</span>
+        
+    </div>
+</section>
